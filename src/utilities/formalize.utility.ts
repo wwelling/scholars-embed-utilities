7398d1f0ca1ba4499a9cd39ac76dc260455bb168
William Welling <wwelling@library.tamu.edu>
2019-08-15 21:56:52 -0500

    more complex formalize regex

--- src/utilities/formalize.utility.ts
+++ src/utilities/formalize.utility.ts
@@ -12,6 +12,17 @@ export const formalize = (input: string | string[], mapping: any = {}) => {
         }
     }
     return input ? (input as string)
-        .replace(/[^\(]([A-Z]+[a-z])/g, ' $1')
-        .replace(/^./, (str) => str.toUpperCase()) : '';
+        .replace(/([a-z])([A-Z][a-z])/g, '$1 $2')
+        .replace(/([A-Z][a-z])([A-Z])/g, '$1 $2')
+        .replace(/([a-z])([A-Z]+[a-z])/g, '$1 $2')
+        .replace(/([A-Z]+)([A-Z][a-z][a-z])/g, '$1 $2')
+        .replace(/([a-z]+)([A-Z0-9]+)/g, '$1 $2')
+
+        .replace(/([A-Z]+)([A-Z][a-rt-z][a-z]*)/g, '$1 $2')
+        .replace(/([0-9])([A-Z][a-z]+)/g, '$1 $2')
+
+        .replace(/([A-Z]{2,})([0-9]{2,})/g, '$1 $2')
+        .replace(/([0-9]{2,})([A-Z]{2,})/g, '$1 $2')
+        .replace(/^./, (match) => match.toUpperCase())
+        .trim() : '';
 };
